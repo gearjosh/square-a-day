11f013377348e592199073496c88324f67143866
Josh Gearheart <other.josh.gearheart@gmail.com>
2021-04-16 13:07:12 -0700

    refactor size multiplier

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ import Square from './Square';
 
 function App() {
   const [drawingArray, setDrawingArray] = useState(drawings);
+  const sizeMultiplier = 7;
 
   const Grid = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(8vw, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(${sizeMultiplier}vw, 1fr));
     /* grid-auto-rows: 1fr; */
     grid-auto-flow: dense;
   `;
@@ -24,7 +25,7 @@ function App() {
   return (
     <Grid>
       {drawingArray.map((obj, i) => {
-        return <Square img={obj.img} alt={obj.title} guestArtist={obj.guestArtist} size={obj.size} key={i} gridSize={obj.size * 8} />;
+        return <Square img={obj.img} alt={obj.title} guestArtist={obj.guestArtist} size={obj.size} key={i} gridSize={obj.size * sizeMultiplier} />;
       })}
     </Grid>
   );
